35f9928896d7595c9ffaa8ca2accfd36bc9909d7
Nguyễn Minh Vương <163238154+MinVuong@users.noreply.github.com>
2024-05-30 13:33:15 +0700

    Create flist.f

---
+++ flist.f
@@ -0,0 +1,12 @@
+00_src/full_adder.sv
+00_src/full_sub.sv
+00_src/adder_8bit.sv
+00_src/sub_8bit.sv
+00_src/and_8bit.sv
+00_src/or_8bit.sv
+
+00_src/not_8bit.sv
+00_src/shift_left_8bit.sv
+00_src/shift_right_8bit.sv
+00_src/alu_top.sv
+00_src/synth_wrapper.v
